b304c3689d8ccba7745ccc577229cad539b604ba
JeffreyR002 <jefreyrodriguez242@gmail.com>
2024-11-10 00:43:23 -0500

    mas cambios

--- views/administrador/usuarios/index.php
+++ views/administrador/usuarios/index.php
@@ -1,169 +1,226 @@
 <?php
-    // Conectar a la base de datos
-    require_once 'config/db.php';
-    $db = Database::connect();
+// Conectar a la base de datos
+require_once 'config/db.php';
+$db = Database::connect();
 ?>
-<?php require_once "views/administrador/Vista/parte_superior.php" ?> 
-<!-- nicio del contenido principal -->
-    <div class="container">
-        <h1>Contenido principal</h1>
-    </div>
+
 <!DOCTYPE html>
-<html>
+<html lang="es">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Usuarios</title>
-    <link rel="stylesheet" type="text/css" href="../assets/styles.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/buttons/2.3.6/css/buttons.dataTables.min.css">
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.3.6/js/dataTables.buttons.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.html5.min.js"></script>
-    <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/buttons/2.3.6/js/buttons.print.min.js"></script>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Sistema Web</title>
+    <link href="../assets/css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
+        crossorigin="anonymous"></script>
 </head>
-<body>
-<header>
-    <div class="logo-container">
-        <img src="../assets/Logo-Sena.jpg" alt="Logo de la empresa" class="logo">
-    </div>
-    <div class="title">
-        <h1>Gestion de Ambientes de formacion</h1>
-    </div>
-    <div class="datetime">
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
         <?php
-        date_default_timezone_set('America/Bogota');
-        $fechaActual = date("d/m/Y");
-        $horaActual = date("h:i a");
+        $url_regresar ='../admin/home';
         ?>
-        <div class="datetime">
-            <div class="fecha">
-                <p>Fecha actual: <?php echo $fechaActual; ?></p>
-            </div>
-            <div class="hora">
-                <p>Hora actual: <?php echo $horaActual; ?></p>
-            </div>
-        </div>
-    </div>
-</header>
-<nav>
-    <div class="column-toggle-buttons">
-        <button class="toggle-vis" data-column="0">ID</button>
-        <button class="toggle-vis" data-column="1">Nombres</button>
-        <button class="toggle-vis" data-column="2">Apellidos</button>
-        <button class="toggle-vis" data-column="3">Correo</button>
-        <button class="toggle-vis" data-column="4">Rol</button>
-        <button class="toggle-vis" data-column="5">Acciones</button>
-    </div>
-</nav>  
-<section class="ambiente" id="section-ambiente">
-    <div class="subtitulo-ambiente">
-        <h2>Usuarios</h2>
-    </div>
-    <div class="descripcion-ambiente">
-        <p>Gestión de Usuarios</p>
-    </div>
-    <div class="tabla-ambientes tabla-scroll">
-        <table class="table table-striped table-dark table_id" border="1" id="tabla-ambientes">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombres</th>
-                    <th>Apellidos</th>
-                    <th>Correo</th>
-                    <th>Rol</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                // Consulta para obtener los datos
-                $query = "SELECT id_usuario, Nombres, Apellidos, Correo, Rol, Estado FROM t_usuarios";
-                $result = $db->query($query);
+        <a class="navbar-brand" href="<?php echo $url_regresar; ?>">GAFRA</a><button
+            class="btn btn-link btn-sm order-1 order-lg-0" id="sidebarToggle" href="#"><i
+                class="fas fa-bars"></i></button>
+        <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
+                    aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                    <a class="dropdown-item" href="#">Configuración</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="login.html">Salir</a>
+                </div>
+            </li>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <a class="nav-link" href="<?php echo $url_regresar; ?>">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Inicio
+                        </a>
+                        <div class="sb-sidenav-menu-heading">Interface</div>
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/admin/areaTrabajo'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Area de trabajo
+                        </a>
 
-                // Mostrar los datos en la tabla
-                if ($result->num_rows > 0) {
-                    while($row = $result->fetch_assoc()) {
-                        echo "<tr>";
-                        echo "<td>" . $row["id_usuario"] . "</td>";
-                        echo "<td>" . $row["Nombres"] . "</td>";
-                        echo "<td>" . $row["Apellidos"] . "</td>";
-                        echo "<td>" . $row["Correo"] . "</td>";
-                        echo "<td>" . $row["Rol"] . "</td>";
-                        echo "<td>";
-                        
-                            $url_update = '/dashboard/gestion%20de%20ambientes/usuarios/updateUsuario/' . $row['id_usuario'];
-                            echo "<a href='" . $url_update . "' class='boton-modificar'><img src='../assets/editar.svg'></a>";
-                        echo "</td>";
-                        echo "</tr>";
-                    }
-                } else {
-                    echo "<tr><td colspan='6'>0 resultados</td></tr>"; // Actualizar el colspan
-                }
-                $db->close();
-                ?>
-            </tbody>
-        </table>
-        <div class="filtro-y-crear">
-            <div class="crear-ambiente">
-                <ul>
-                    <?php
-                    $url_create = '/dashboard/gestion%20de%20ambientes/usuarios/createUsuario/';
-                    ?>
-                    <li><a href="<?php echo $url_create; ?>" id="btn-create">Crear Nuevo Usuario</a></li>
-                </ul>
-            </div>
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/usuarios/usuarios'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Gestión de Usuarios
+                        </a>
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/admin/reportes'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Gestión de Reportes
+                        </a>
+
+                        <div class="sb-sidenav-menu-heading">Interface</div>
+
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/admin/computadores'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Computadores
+                        </a>
+
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/proveedores/proveedores'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Proveedores
+                        </a>
+
+                        <a class="nav-link" href='/dashboard/gestion%20de%20ambientes/Producto/listarProductos'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Productos
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
         </div>
-        <div class="regresar">
-            <?php
-            $url_regresar = '../admin/home';
-            ?>
-            <a href="<?php echo $url_regresar; ?>" class="button boton-centrado" id="btn-regresar">Regresar</a>
-        </div>
-        <div class="salir">
-            <button id="btn_salir">Salir</button>
+        <div id="layoutSidenav_content">
+            <main>
+                <div class="container-fluid">
+                    <h1 class="mt-4">Gestion de Inventarios Gafra</h1>
+                    <ol class="breadcrumb mb-4">
+                        <li class="breadcrumb-item active">Menu</li>
+                    </ol>
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <?php
+                                    $url_create = '/dashboard/gestion%20de%20ambientes/usuarios/createUsuario/';
+                                    ?>
+                                    <a class="small text-white stretched-link" href="<?php echo $url_create; ?>"
+                                        id="btn-create">Crear Nuevo Usuario</a>
+                                    <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card mb-4">
+                        <div class="card-header"><i class="fas fa-table mr-1"></i>Gestión de Usuarios</div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered" id="tabla-reportes" id="dataTable" width="100%"
+                                    cellspacing="0">
+                                    <thead>
+                                        <!-- Botón para mostrar/ocultar el menú de filtros -->
+                                        <button class="filter-button" onclick="toggleFilterMenu()">Mostrar
+                                            Filtros</button>
+
+                                        <!-- Menú de filtros lateral -->
+                                        <div class="filter-menu" id="filterMenu" style="display:none;">
+                                            <h3>Filtrar Columnas</h3>
+                                            <button class="toggle-vis" data-column="0">ID</button>
+                                            <button class="toggle-vis" data-column="1">Nombres</button>
+                                            <button class="toggle-vis" data-column="2">Apellidos</button>
+                                            <button class="toggle-vis" data-column="3">Correo</button>
+                                            <button class="toggle-vis" data-column="4">Rol</button>
+                                            <button class="toggle-vis" data-column="5">Acciones</button>
+                                        </div>
+                                        <tr>
+                                            <th>ID</th>
+                                            <th>Nombres</th>
+                                            <th>Apellidos</th>
+                                            <th>Correo</th>
+                                            <th>Rol</th>
+                                            <th>Acciones</th>
+                                        </tr>
+                                    <tbody>
+                                        <?php
+                                        // Consulta para obtener los datos
+                                        $query = "SELECT id_usuario, Nombres, Apellidos, Correo, Rol, Estado FROM t_usuarios";
+                                        $result = $db->query($query);
+
+                                        // Mostrar los datos en la tabla
+                                        if ($result->num_rows > 0) {
+                                            while ($row = $result->fetch_assoc()) {
+                                                echo "<tr>";
+                                                echo "<td>" . $row["id_usuario"] . "</td>";
+                                                echo "<td>" . $row["Nombres"] . "</td>";
+                                                echo "<td>" . $row["Apellidos"] . "</td>";
+                                                echo "<td>" . $row["Correo"] . "</td>";
+                                                echo "<td>" . $row["Rol"] . "</td>";
+                                                echo "<td>";
+
+                                                $url_update = '/dashboard/gestion%20de%20ambientes/usuarios/updateUsuario/' . $row['id_usuario'];
+                                                echo "<a href='" . $url_update . "' class='boton-modificar'><img src='../assets/editar.svg'></a>";
+                                                echo "</td>";
+                                                echo "</tr>";
+                                            }
+                                        } else {
+                                            echo "<tr><td colspan='6'>0 resultados</td></tr>"; // Actualizar el colspan
+                                        }
+                                        $db->close();
+                                        ?>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+            <footer class="py-4 bg-light mt-auto">
+                <div class="container-fluid">
+                    <div class="d-flex align-items-center justify-content-between small">
+                        <div class="text-muted">Copyright &copy; Your Website 2019</div>
+                        <div>
+                            <a href="#">Privacy Policy</a>
+                            &middot;
+                            <a href="#">Terms &amp; Conditions</a>
+                        </div>
+                    </div>
+                </div>
+            </footer>
         </div>
     </div>
-</section>
-<script>
-    $(document).ready(function() {
-        var table = $('#tabla-ambientes').DataTable({
-            dom: 'Bfrtip',
-            buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
-            paging: true,
-            pageLength: 10 // Mostrar 10 registros por página
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"
+        crossorigin="anonymous"></script>
+    <script src="../assets/Js/scripts.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+    <script src="../assets/demo/chart-area-demo.js"></script>
+    <script src="../assets/demo/chart-bar-demo.js"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+    <script src="../assets/demo/datatables-demo.js"></script>
+    <script>
+        $(document).ready(function () {
+            // Inicializar DataTable con opciones
+            var table = $('#tabla-reportes').DataTable({
+                dom: 'Bfrtip',
+                buttons: ['copy', 'csv', 'excel', 'pdf', 'print'],
+                paging: true,
+                pageLength: 5
+            });
+
+            // Evento para mostrar/ocultar columnas al hacer clic en los botones de filtro
+            $('button.toggle-vis').on('click', function (e) {
+                e.preventDefault();
+                var columnIdx = $(this).attr('data-column'); // Obtener el índice de la columna
+                table.column(columnIdx).visible(!table.column(columnIdx).visible()); // Alternar visibilidad
+            });
         });
 
-        // Configuración de los botones para mostrar/ocultar columnas
-        $('.toggle-vis').on('click', function(e) {  
-            e.preventDefault();
-
-            // Obtenemos el índice de la columna correspondiente al botón
-            var columnIdx = parseInt($(this).attr('data-column'));
-
-            // Obtenemos el estado de visibilidad de la columna y lo invertimos
-            var column = table.column(columnIdx);
-            column.visible(!column.visible());
-        });
-    });
-    
-    function confirmarHabilitar(id) {
-        if (confirm('¿Está seguro de que desea habilitar este usuario?')) {
-            window.location.href = "inhabilitarUsuario/" + id;}
-    }
-
-    function confirmarInhabilitar(id) {
-        if (confirm('¿Está seguro de que desea inhabilitar este usuario?')) {
-            window.location.href = "habilitarUsuario/" + id;}
-    }
-</script>
-
-<footer>
-    <p>Sena todos los derechos reservados</p>
-</footer>
+        // Función para mostrar/ocultar el menú de filtros
+        function toggleFilterMenu() {
+            var menu = document.getElementById("filterMenu");
+            menu.style.display = menu.style.display === "none" || menu.style.display === "" ? "block" : "none";
+        }
+    </script>
 </body>
+
 </html>
-<!-- fin del contenido principal -->
-<?php require_once "views/administrador/Vista/parte_inferior.php" ?> 
