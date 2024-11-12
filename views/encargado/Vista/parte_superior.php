bc149a4785f94db8229472e952f00637ffd695f2
JeffreyR002 <jefreyrodriguez242@gmail.com>
2024-11-12 14:25:32 -0500

    h

---
+++ views/encargado/Vista/parte_superior.php
@@ -0,0 +1,80 @@
+<?php
+session_start();
+
+// Comprobar si el usuario está autenticado
+if (!isset($_SESSION['Id_usuario'])) {
+    header("Location: /dashboard/gestion%20de%20ambientes/login");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Sistema Web</title>
+    <link href="../assets/css/styles.css" rel="stylesheet" />
+    <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
+        crossorigin="anonymous" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js"
+        crossorigin="anonymous"></script>
+</head>
+
+<body class="sb-nav-fixed">
+    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
+        <?php
+        $url_regresar = '../../../encargado/home/';
+        ?>
+        <a class="navbar-brand" href="<?php echo $url_regresar; ?>">
+            <img src="../assets/img/login0.png" class="logo" style="width: 150px; height: auto; max-height: 50px;">
+        </a>
+        <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+            <ul class="navbar-nav ml-auto mr-0 mr-md-3 my-2 my-md-0">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" id="userDropdown" href="#" role="button" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="userDropdown">
+                        <a class="dropdown-item" href="#">Configuración</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="/dashboard/gestion%20de%20ambientes/login">Salir</a>
+                    </div>
+                </li>
+            </ul>
+        </ul>
+    </nav>
+    <div id="layoutSidenav">
+        <div id="layoutSidenav_nav">
+            <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+                <div class="sb-sidenav-menu">
+                    <div class="nav">
+                        <a class="nav-link" href="<?php echo $url_regresar; ?>">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Inicio
+                        </a>
+
+                        <div class="sb-sidenav-menu-heading">Interface</div>
+
+                        <a class="nav-link" href="/dashboard/gestion%20de%20ambientes/proveedores/proveedores">
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Proveedores
+                        </a>
+
+                        <a class="nav-link"
+                            href='/dashboard/gestion%20de%20ambientes/inventario/listarEntradasAdministrador'>
+                            <div class="sb-nav-link-icon"><i class="fas fa-chart-area"></i></div>
+                            Inventario
+                        </a>
+                    </div>
+                </div>
+                <div class="sb-sidenav-footer">
+                    <div class="small">Logged in as:</div>
+                    Start Bootstrap
+                </div>
+            </nav>
+        </div>
+        <div id="layoutSidenav_content">
