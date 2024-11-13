31c9b03ce34371a5db8e0420f991a83040d9881a
JeffreyR002 <jefreyrodriguez242@gmail.com>
2024-11-13 00:34:46 -0500

    ult

--- views/administrador/ambientes/update.php
+++ views/administrador/ambientes/update.php
@@ -364,7 +364,6 @@
                         </div>
                     </div>
                 </div>
-
                 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
                 <script>
                     function showConfirmModal(event) {
