8dc36d9c2f41fa616409c4ab62e74321dee03bc7
JeffreyR002 <jefreyrodriguez242@gmail.com>
2024-11-14 17:08:09 -0500

    d

--- views/encargado/reportes/create.php
+++ views/encargado/reportes/create.php
@@ -8,7 +8,7 @@
     <meta name="description" content="" />
     <meta name="author" content="" />
     <title>Sistema Web</title>
-    <link rel="icon" href="../../assets/img/login02.ico" type="image/x-icon">
+    <link rel="icon" href="../assets/img/login02.ico" type="image/x-icon">
     <link href="../assets/css/styles.css" rel="stylesheet" />
     <link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet"
         crossorigin="anonymous" />
